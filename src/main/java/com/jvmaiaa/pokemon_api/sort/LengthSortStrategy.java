7dbc5bb170c09000349f0f67a080882e6fade8c6
jvmaiaa <joaovictormaia482@gmail.com>
2025-01-19 04:45:29 -0300

    ♻️refactor: Refatora a ordenação por quantidade de letras na palavra e adiciona comentários sobre complexidade

--- src/main/java/com/jvmaiaa/pokemon_api/sort/LengthSortStrategy.java
+++ src/main/java/com/jvmaiaa/pokemon_api/sort/LengthSortStrategy.java
@@ -5,18 +5,26 @@ import java.util.List;
 public class LengthSortStrategy implements SortStrategy {
 
     @Override
-    public List<String> sort(List<String> pokemons) {
-        // Implementação manual de Bubble Sort para ordenação por tamanho
-        int n = pokemons.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (pokemons.get(j).length() > pokemons.get(j + 1).length()) {
-                    String temp = pokemons.get(j);
-                    pokemons.set(j, pokemons.get(j + 1));
-                    pokemons.set(j + 1, temp);
+    public List<String> sort(List<String> listaDePokemons) {
+        int n = listaDePokemons.size(); // n
+        for (int i = 0; i < n - 1; i++) { // n - 1
+            for (int j = 0; j < n - i - 1; j++) { // n^2 - 1
+                if (listaDePokemons.get(j).length() > listaDePokemons.get(j + 1).length()) { // n^2 - 1
+                    trocaPosicao(listaDePokemons, j, j+1); // n^2
                 }
             }
         }
-        return pokemons;
+        return listaDePokemons; // 1
     }
+    // G(n) = n + n - 1 + n^2 - 1 + n^2 - 1 + n^2 + 1
+    // G(n) = 3n^2 + 2n + 1 - 2
+    // O(n) = n^2 -> esse vai ser o resultado da complexidade do código no PIOR CASO
+
+    private void trocaPosicao(List<String> lista, int i, int j){
+        String variavelAuxiliar = lista.get(i); // 1
+        lista.set(i, lista.get(j)); // 1 - substitui o elemento do índice atual pelo elemento do próximo índice
+        lista.set(j, variavelAuxiliar); // 1
+    }
+    // G(n) = 1 + 1 + 1
+    // O(1) -> esse vai ser o resultado da complexidade do código no PIOR CASO
 }
